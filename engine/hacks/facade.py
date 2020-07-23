5f3fb22ddc351a1b0861f08ef950f734c1433c36
Shunyi Chen <526637060@qq.com>
2020-07-23 20:43:21 +0800

    Add a new hack function - invoke_shell

--- engine/hacks/facade.py
+++ engine/hacks/facade.py
@@ -15,9 +15,6 @@ class Facade(object):
     def close_ssh(self):
         self.ssh_instance.close_ssh()
 
-    def exec_cmd(self, cmd):
-        self.ssh_instance.exec_cmd(cmd)
-
     def open_pod(self):
         self.ssh_instance.open_pod()
     
@@ -27,11 +24,21 @@ class Facade(object):
     def close_pod(self):
         self.ssh_instance.close_pod()
 
+    def invoke_shell(self, cmd):
+        return self.ssh_instance.invoke_shell(cmd)
+
     def debug(self, msg):
         return self.debug_instance.debug(msg)
 
     def add(self, x, y):
         return self.debug_instance.add(x, y)
+
+    def quit_procedure(self): # 超时后的处理函数
+        exit(1)
+    
+    def continue_warning(self, msg): # 超时后的处理函数
+        print(msg)
+        pass
 
 if __name__ == '__main__':
     f = Facade()
--- engine/hacks/ssh.py
+++ engine/hacks/ssh.py
@@ -11,15 +11,9 @@ class SSH(object):
         self.username = username
         self.password = password
         self.key = None
-
         if key:
             self.key = paramiko.RSAKey.from_private_key_file(key, password)
-
-        self.pty = True
         self.ssh = None
-        self.sftp = None
-        self.client = None
-
         self.pod_in = None
         self.pod_out = None
         self.pod_err = None
@@ -32,66 +26,41 @@ class SSH(object):
         except socket.error:
             print('SSH connection failed.')
         return self.ssh
-
-    def set_pty(self, pty):
-        self.pty = pty
-
-    def exec_cmd(self, cmd):
-        command_in, command_out, command_error = self.ssh.exec_command(cmd, get_pty=self.pty)
-        result = command_out.read().strip()
-        print(result)
-        return result
-
+ 
     def close_ssh(self):
         self.ssh.close()
 
     def open_pod(self):
         self.pod_in, self.pod_out, self.pod_err = self.ssh.exec_command('sudo su')
-        return self._read_pod_cmd_output(2)
+        return self._read_pod_cmd_output()
 
     def exec_pod_cmd(self, cmd):
         self.pod_in.channel.send(cmd + os.linesep)
         return self._read_pod_cmd_output()
 
-    def _read_pod_cmd_output(self, timeout=30):
+    def _read_pod_cmd_output(self):
         time.sleep(0.5)
-        time_count = 0
-        while not self.pod_out.channel.recv_ready():
-            time.sleep(0.01)
-            time_count = time_count + 0.01
-            if time_count > timeout:
-                break
-
         result = ""
         while self.pod_out.channel.recv_ready():
             result += str(self.pod_out.channel.recv(1024), encoding ="utf-8")
-
         print(result)
         return result.strip()
 
     def close_pod(self):
         self.pod_in.channel.send("exit" + os.linesep)
-
-    def sftp(self):
-        try:
-            self.client = paramiko.Transport((self.host, 22))
-        except Exception as error:
-            print('Connect as sftp failed.')
-        else:
-            try:
-                self.client.connect(username=self.username, password=self.password, pkey=self.key)
-            except Exception as error:
-                print('Connect as sftp failed.')
-            else:
-                self.sftp = paramiko.SFTPClient.from_transport(self.client)
-        return self.sftp
-
-    def close_sftp(self):
-        self.sftp.close()
-
+    
+    def invoke_shell(self, cmd):
+        channel = self.ssh.invoke_shell()
+        channel.send(cmd+'\n')
+        time.sleep(3)
+        output = channel.recv(2024)
+        output = output.decode('utf8')
+        # print(output)
+        return output
+        # time.sleep(10)
+        # stdout = channel.recv(1024*100000)
+        # out_list = stdout.decode().split("\n")
+        # print(out_list)
+ 
 if __name__ == '__main__':
     pass
-    # ssh = SSH('tyserccd901', 'eccd', key='/home/omts/jenkins/ssh_key/forDirector/id_rsa')
-    # ssh.open_ssh()
-    # print(ssh.exec_cmd("pwd"))
-    # ssh.close_ssh()
